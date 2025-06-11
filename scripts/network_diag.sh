90e830426077911a7da2e93bffd08fd053fc408d
Harsha-604 <v.harsha2104@gmail.com>
2025-06-11 20:03:29 +0530

    moved file in local directory

--- scripts/network_diag.sh
+++ scripts/network_diag.sh
@@ -1,4 +1,15 @@
-read -p "Enter Domain"  Dom
+read -p "Enter Domain:" dom
 
-echo "Domain:"$Dom
+echo "Domain is:" $dom
 
+echo -e "\n IP Address:" 
+hostname -I
+
+echo -e "\nPinging" $dom ":"
+ping -c 4 $dom
+
+echo -e "\nDNS Resolution:"
+dig +short $dom
+
+echo -e "\nTraceroute to :"$dom
+traceroute $dom
